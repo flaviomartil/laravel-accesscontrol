e34e18759ca058e97f9c347237c8b728e22243dd
flaviomartil <flaviomartil5@gmail.com>
2024-05-17 17:24:28 -0300

    Change AccessControlProvider.php

---
+++ src/assets/migrations/2024_05_17_082708_create_permission_role_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('permission_role');
+        Schema::create('permission_role', function (Blueprint $table) {
+            $table->uuid('permission_id');
+            $table->uuid('role_id');
+            $table->timestamps();
+
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permission_role');
+    }
+};
